cd4562b07378997652499d9158154ffc1d50bdee
Anubhav Gupta <2005853@kiit.ac.in>
2023-03-30 23:03:05 +0530

    changes

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const routes = require("./routes/Todoroute");
 const express=require("express");
 const app = express();
 const cors=require('cors');
-
+const PORT=process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 app.use(routes);
-app.listen(8000,()=>{
+app.listen(PORT,()=>{
     console.log("Listening on port 8000")
 })
 
